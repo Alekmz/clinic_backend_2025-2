a8bf3faa08c2c98ea985a900c6f8a3ff0f214bbe
Alekmz <amalexmuniz@gmail.com>
2026-09-01 21:46:25 -0300

    endpoints crud

--- api_clinica/src/app.ts
+++ api_clinica/src/app.ts
@@ -1,6 +1,6 @@
 import express, { json, type Express, type Request, type Response } from 'express';
 import { db } from './prisma/db';
-import bcrypt  from 'bcrypt';
+import bcrypt from 'bcrypt';
 
 
 const app: Express = express();
@@ -12,9 +12,20 @@ app.get('/usuarios', async (_: Request, res: Response) => {
   res.json(users);
 });
 
+app.get('/usuarios/:id', async (req: Request, res: Response) => {
+  const id = Number(req.params.id)
+  const usuario = await db.orm.public.User.where({ id }).first()
+  if (usuario) {
+    res.json(usuario);
+  } 
+
+  res.status(400).send("Usuário não existe!")
+
+});
+
 app.post('/usuario', async (req: Request, res: Response) => {
   const body = req.body;
-  const hashSenha =  await bcrypt.hash(body.password, 10)
+  const hashSenha = await bcrypt.hash(body.password, 10)
   const usuario = await db.orm.public.User.create({
     email: body.email,
     name: body.name,
@@ -26,6 +37,43 @@ app.post('/usuario', async (req: Request, res: Response) => {
     "mensagem": "Usuário Criado!",
     "data": usuario
   });
+});
+
+
+app.put('/usuarios/:id', async (req: Request, res: Response) => {
+  const body = req.body;
+  const id = Number(req.params.id)
+
+  const hashSenha = await bcrypt.hash(body.password, 10)
+  const usuario = await db.orm.public.User.where({ id }).update({
+    email: body.email,
+    name: body.name,
+    password: hashSenha,
+    username: body.username
+  });
+  if (usuario) {
+    res.json({
+      "mensagem": "Usuário atualizado com sucesso!",
+      "data": usuario
+    });
+
+  }
+  res.status(400).send("Usuário não encontrado!")
+
+});
+
+
+app.delete('/usuarios/:id', async (req: Request, res: Response) => {
+  const id = Number(req.params.id)
+  const usuario = await db.orm.public.User.where({ id }).delete()
+  if (usuario) {
+    res.json({
+      mensagem: "Usuário deletado",
+      data: usuario
+    });
+  } else {
+    res.status(400).send("Usuário não existe!")
+  }
 });
 
 app.listen(3000, () => {
