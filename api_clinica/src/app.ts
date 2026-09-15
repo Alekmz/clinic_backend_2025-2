dee1b84455e5a0566aee7dce3cb5ae5de3538db5
Alekmz <amalexmuniz@gmail.com>
2026-09-14 21:45:47 -0300

    jwt e login

--- api_clinica/src/app.ts
+++ api_clinica/src/app.ts
@@ -1,6 +1,7 @@
 import express, { json, type Express, type Request, type Response } from 'express';
 import { db } from './prisma/db';
 import bcrypt from 'bcrypt';
+import { signTokenAcesso, signTokenRefresh } from './utils/jwt';
 
 
 const app: Express = express();
@@ -25,7 +26,8 @@ app.get('/usuarios/:id', async (req: Request, res: Response) => {
 
 app.post('/usuario', async (req: Request, res: Response) => {
   const body = req.body;
-  const hashSenha = await bcrypt.hash(body.password, 10)
+  const hashSenha = await bcrypt.hash(body.password, 10);
+  
   const usuario = await db.orm.public.User.create({
     email: body.email,
     name: body.name,
@@ -75,6 +77,57 @@ app.delete('/usuarios/:id', async (req: Request, res: Response) => {
     res.status(400).send("Usuário não existe!")
   }
 });
+
+
+app.post("/login", async (req: Request, res: Response)=>{
+  const body = req.body;
+  const existeUsuario =  await db.orm.public.User.where({ email: body.email }).first()
+  if(existeUsuario){
+    const senha = body.password
+    const senhaCorreta = await bcrypt.compare(senha, existeUsuario.password)
+    if(senhaCorreta){
+      const tokenAcesso = signTokenAcesso({
+        nome: existeUsuario.name,
+        email: existeUsuario.email
+      })
+      const tokenRefresh = signTokenRefresh({
+        nome: existeUsuario.name,
+        email: existeUsuario.email
+      })
+      
+      const acessoExpiraEm = new Date()
+      const acessoExpiraEm5Minutos = acessoExpiraEm.setMinutes(acessoExpiraEm.getMinutes() + 5)
+      await db.orm.public.Token.create({
+        token:tokenAcesso,
+        type: "ACESSO",
+        revoked: false,
+        usuarioId: existeUsuario.id,
+        expiresAt: new Date(acessoExpiraEm5Minutos).toISOString()
+      });
+
+      const refreshExpiraEm = new Date()
+      const refreshExpiraEm1Dia = refreshExpiraEm.setHours(refreshExpiraEm.getHours() + 24)
+      await db.orm.public.Token.create({
+        token: tokenRefresh,
+        type: "REFRESH",
+        revoked: false,
+        usuarioId: existeUsuario.id,
+        expiresAt: new Date(refreshExpiraEm1Dia).toISOString()
+      });
+
+      res.status(200).json(
+        {"message": "Usuário logado!", 
+          "data": {
+            tokenAcesso,
+            tokenRefresh
+          }
+        })
+    }
+    res.status(401).json({"message": "Senha incorreta!"})
+  }
+  res.status(401).json({"message": "Usuário não encontrado!"})
+
+})
 
 app.listen(3000, () => {
   console.log("Rodando em http://localhost:3000")
